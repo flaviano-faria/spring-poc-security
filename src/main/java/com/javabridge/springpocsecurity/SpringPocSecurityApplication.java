2081f76bcf0611f244e97df58a303386d0d614a5
flaviano-faria <flsilv89@yahoo.com.br>
2026-01-30 21:42:01 -0300

    Initial commit

---
+++ src/main/java/com/javabridge/springpocsecurity/SpringPocSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.javabridge.springpocsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPocSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPocSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javabridge/springpocsecurity/SpringPocSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javabridge.springpocsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPocSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
