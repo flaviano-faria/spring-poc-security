bc5ae51d1f724182a23aa7f36701f344e188dd38
flaviano-faria <flsilv89@yahoo.com.br>
2026-02-01 23:04:41 -0300

    Implementing UserRepository

---
+++ src/main/java/com/javabridge/springpocsecurity/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.javabridge.springpocsecurity.repository;
+
+import com.javabridge.springpocsecurity.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+}
