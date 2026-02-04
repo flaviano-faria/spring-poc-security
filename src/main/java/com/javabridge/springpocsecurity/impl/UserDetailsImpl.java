3af5616bf60cb4da84897ab7081d7b162a569c0d
flaviano-faria <flsilv89@yahoo.com.br>
2026-02-04 20:48:02 -0300

    Implementing UserDetailsServiceImpl

--- src/main/java/com/javabridge/springpocsecurity/impl/UserDetailsImpl.java
+++ src/main/java/com/javabridge/springpocsecurity/impl/UserDetailsImpl.java
@@ -1,13 +1,15 @@
 package com.javabridge.springpocsecurity.impl;
 
+import com.javabridge.springpocsecurity.entities.User;
 import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class UserDetailsImpl implements UserDetails {
@@ -16,6 +18,9 @@ public class UserDetailsImpl implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
+        user.getRoles().stream().map(role ->
+                        new SimpleGrantedAuthority(role.getName().name()))
+                .collect(Collectors.toList());
         return List.of();
     }
 
---
+++ src/main/java/com/javabridge/springpocsecurity/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.javabridge.springpocsecurity.impl;
+
+import com.javabridge.springpocsecurity.entities.User;
+import com.javabridge.springpocsecurity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username).orElseThrow(() -> new RuntimeException("User not found"));
+        return new UserDetailsImpl(user);
+    }
+}
